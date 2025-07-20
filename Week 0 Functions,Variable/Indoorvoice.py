345edbfc03dcb637afc0823cea9016dddeaf7f3c
Atharva Admile <124397305+AtharvaAdmile@users.noreply.github.com>
2025-07-20 17:53:56 +0530

    Add files via upload

---
+++ Week 0 Functions,Variable/Indoorvoice.py
@@ -0,0 +1,6 @@
+# Take Input from user and store it in string variable 'users_input'
+
+users_input=input("Enter some UPPERCASE text here: ")
+
+# Converting text into lowercase and print
+print(users_input.lower())
---
+++ Week 0 Functions,Variable/playback.py
@@ -0,0 +1,5 @@
+# Take Input from user and store it in string variable 'users_input'
+
+users_input=input("Enter some text here: ").replace(" ","...")
+
+print(users_input)
